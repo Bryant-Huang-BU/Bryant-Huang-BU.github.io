bb1349743b6ff9eafb4d8086ac36326574eee11a
killerbacon34 <alex_devries1@baylor.edu>
2023-11-30 16:58:59 -0600

    c

---
+++ loadAllCsvs/Users.py
@@ -0,0 +1,57 @@
+from sqlalchemy.orm import DeclarativeBase, sessionmaker
+from sqlalchemy import Column, Integer, String, create_engine, Double
+import csi3335F2023 as conf
+import pandas as pd
+class Base(DeclarativeBase):
+    pass
+
+
+class Users(Base):
+    __tablename__ = "users"
+
+    ID = Column(Integer, primary_key=True)
+    username = Column(String(255))
+    password = Column(String(255))
+
+csv_file = 'Users.csv'
+
+engineStr = ("mysql+pymysql://" +
+             conf.mysql['username'] + ":" +
+             conf.mysql['password'] + "@" +
+             conf.mysql['location'] + ":3306/" +
+             conf.mysql['database'])
+
+# Creating the Database Engine and Tables
+engine = create_engine(engineStr)
+Base.metadata.create_all(engine)
+
+# Creating a Session to Interact with the Database
+Session = sessionmaker(bind=engine)
+session = Session()
+
+df = pd.read_csv(csv_file)
+allUsers = session.query(Users).all()
+
+for index, row in df.iterrows():
+    foundMatch = False
+    # Replace NaN values with None
+    row = row.where(pd.notna(row), None)
+
+    # Create a new object of the class with data from the row
+    new_row = Users(**row.to_dict())
+
+    # Remove the previous instance of the player
+    for person in allUsers:
+        if new_row.ID == person.ID:
+            foundMatch = True
+            session.delete(person)
+            break
+
+    # Add the new object to the session
+    session.add(new_row)
+    if not foundMatch:
+        if new_row.username is not None and new_row.password is not None:
+            print(new_row.username + " " + new_row.password)
+
+session.commit()
+
