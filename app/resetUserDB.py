a89bc6d06db2d79afbb044742166108cda2618d2
James Truitt <James_Truitt1@baylor.edu>
2023-12-02 11:15:12 -0600

    Logging update

--- app/resetUserDB.py
+++ app/resetUserDB.py
@@ -5,8 +5,11 @@ from app.models import Users
 def reset():
     with app.app_context():
         try:
-            db.reflect()
-            db.drop_all()
+            print("Trying to drop table if it exists")
+            Users.__table__.drop(db.engine)
+            print("Table dropped")
             db.create_all()
+            print("Create correct table")
         except:
+            print("Creating new table")
             db.create_all()
